1961920e101f2902c6c7e2d161fb014cec51c295
Patrick <me@patrickphat.com>
2022-10-10 17:34:37 +0700

    Update

--- plugins/init.lua
+++ plugins/init.lua
@@ -26,4 +26,16 @@ return {
         require "custom.plugins.lspconfig"
       end,
   },
+  ["nvim-treesitter/nvim-treesitter"] = {
+    module = "nvim-treesitter",
+    setup = function()
+      require("core.lazy_load").on_file_open "nvim-treesitter"
+    end,
+    cmd = require("core.lazy_load").treesitter_cmds,
+    run = ":TSUpdate",
+    config = function()
+      require "plugins.configs.treesitter"
+      require "custom.configs.treesitter"
+    end,
+  },
 }
